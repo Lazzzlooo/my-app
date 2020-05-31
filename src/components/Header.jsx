123a07dc9a17c0945199609003b09f59384bca1c
Vlad Kaprelev <vladkaprelev@gmail.com>
2020-05-31 16:37:15 +0700

    Создан элемент Header, настроенно позиционирование элементов

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <ul className="header__list">
+                <li className="header__item">
+                    <a className="header__logo logo" href="#">Chat</a>
+                </li>
+                <li className="header__item">
+                    <div className="header__search search">
+                        <form action="" className="search-form">
+                            <input type="text" placeholder={"Search"}/>
+                        </form>
+                    </div>
+                </li>
+                <li className="header__item">
+                    <a href="#" className="header__user">User Name</a>
+                    <img src="/src/img/Photo.png" alt="" width={40} height={40}/>
+                </li>
+            </ul>
+        </header>
+
+    );
+}
+
+export default Header;
