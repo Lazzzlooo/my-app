65e24e8e1edba40674b1e31f9fd2e6186aaa0f78
Vlad Kaprelev <vladkaprelev@gmail.com>
2020-06-04 18:45:37 +0700

    Добавлены размеры и отступы для Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,58 +1,59 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <section className="profile container">
-            <ul className="profile__list">
-                <li className="profile__item">
-                    <p className="profile__avatar">
+        <section className={s.profile}>
+            <ul className={s.list}>
+                <li className={s.item}>
+                    <p className={s.avatar}>
                         <img src="https://img.icons8.com/plasticine/2x/user.png" width={200} height={200} alt=""/>
                     </p>
-                    <div className="profile__user-descr">
-                        <p className="profile__name">User Name</p>
-                        <p className="profile__working">User Job</p>
+                    <div className={s.user_descr}>
+                        <p className={s.name}>User Name</p>
+                        <p className={s.working}>User Job</p>
                     </div>
-                    <ul className="profile__social-list">
-                        <li className="profile__social-item"><a href="#">Facebook</a></li>
-                        <li className="profile__social-item"><a href="#">Twitter</a></li>
-                        <li className="profile__social-item"><a href="#">Instagram</a></li>
-                        <li className="profile__social-item"><a href="#">Linkedin</a></li>
+                    <ul className={s.social_list}>
+                        <li className={s.social_item}><a href="#">Facebook</a></li>
+                        <li className={s.social_item}><a href="#">Twitter</a></li>
+                        <li className={s.social_item}><a href="#">Instagram</a></li>
+                        <li className={s.social_item}><a href="#">Linkedin</a></li>
                     </ul>
-                    <div className="profile__buttons">
-                        <a href="" className="profile__button-message">Message</a>
-                        <button className="profile__button-more">More</button>
+                    <div className={s.buttons}>
+                        <button className={s.button_message}>Message</button>
+                        <button className={s.button_more}>More</button>
                     </div>
                 </li>
-                <li className="profile__item">
-                    <div className="profile__info">
-                        <p className="profile__online">last seen one hour ago</p>
-                        <ul className="profile__about">
-                            {/*Ключ - значение ?*/}
-                            <li className="profile__about--key">
+                <li className={s.item}>
+                    <div className={s.info}>
+                        <p className={s.online}>last seen one hour ago</p>
+                        <ul className={s.about}>
+                            {/*Ключ _ значение ?*/}
+                            <li className={s.about_key}>
                                 <p>Birthday:</p>
                                 <p>Current city:</p>
                                 <p>Website:</p>
                             </li>
-                            <li className="profile__about--value">
+                            <li className={s.about_value}>
                                 <a href={"#"}>February 20, 2000</a>
                                 <a href={"#"}>New-York</a>
                                 <a href={"#"}>www.x.com</a>
                             </li>
                         </ul>
-                        <div className="profile__photo">
-                            <p className="profile__title">My Photo</p>
-                            <ul className="profile__photo-list">
-                                <li className="profile__photo-item">
+                        <div className={s.photo}>
+                            <p className={s.title}>My Photo</p>
+                            <ul className={s.photo_list}>
+                                <li className={s.photo_item}>
                                     <a href="#">
                                         <img src="https://via.placeholder.com/94" alt=""/>
                                     </a>
                                 </li>
-                                <li className="profile__photo-item">
+                                <li className={s.photo_item}>
                                     <a href="#">
                                         <img src="https://via.placeholder.com/94" alt=""/>
                                     </a>
                                 </li>
-                                <li className="profile__photo-item">
+                                <li className={s.photo_item}>
                                     <a href="#">
                                         <img src="https://via.placeholder.com/94" alt=""/>
                                     </a>
