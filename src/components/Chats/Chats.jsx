64f934926f9494ea69c69cd925293d808c6bd480
Vlad Kaprelev <vladkaprelev@gmail.com>
2020-06-12 22:15:23 +0700

    добавлен элемент Chats

---
+++ src/components/Chats/Chats.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Chats.module.css';
+
+const Chats = () => {
+  return (
+    <div className={s.chats}>
+      Chats
+    </div>
+  )
+}
+
+export default Chats;
