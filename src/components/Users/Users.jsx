c6251ea657362af497f8982266b85ccc1c83d4c8
Vlad Kaprelev <vladkaprelev@gmail.com>
2020-09-03 13:10:06 +0700

    получение списка пользователей с сервера

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import s from './Users.module.scss';
+import * as axios from 'axios';
+import avatar from '../../img/avatar.png'
 
 
 const Users = (props) => {
+  if (props.users.length === 0) {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(resposive => {
+      props.setUsers(resposive.data.items);
+    })
+  }
 
-debugger;
   return (
     <section className={s.users}>
       <ul>
         {props.users.map(u => <div key={u.id}>
           <span>
             <div>
+              <img src={u.photos.small != null ? u.photos.small : avatar} alt="" width={75}/>
             </div>
-            <div><button onClick={() => {props.toggleFollow(u.id)}}>{u.followed? 'Unfollow' : 'Follow'}</button></div>
+            <div><button onClick={() => {props.toggleFollow(u.id)}}>{u.followed ? 'Unfollow' : 'Follow'}</button></div>
           </span>
           <span>
             <span>
-              <div>{u.firstName} {u.lastName}</div>
-              <div>{u.status}</div>
+              <span>{u.name}</span>
+              <span>{u.status}</span>
+              <div>{u.id}</div>
             </span>
             <span>
-              <div>{u.location.country}</div><div>{u.location.city}</div>
+              {/*<div>{u.location.country}</div><div>{u.location.city}</div>*/}
             </span>
           </span>
         </div>)}
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,62 +4,62 @@ const SET_USERS = 'SET-USERS';
 
 let initialState = {
   users: [
-    {
-      id: 1,
-      followed: true,
-      firstName: 'Ivan',
-      lastName: 'Alekseev',
-      location: {country: 'Russia', city: 'Tomsk'},
-      status: 'I\'m fine'
-    },
-    {
-      id: 2,
-      followed: false,
-      firstName: 'Alex',
-      lastName: 'Alekseev',
-      location: {country: 'Russia', city: 'Tomsk'},
-      status: 'I\'m fine'
-    },
-    {
-      id: 3,
-      followed: false,
-      firstName: 'Sam',
-      lastName: 'Alekseev',
-      location: {country: 'Russia', city: 'Moscow'},
-      status: 'I\'m fine'
-    },
-    {
-      id: 4,
-      followed: true,
-      firstName: 'John',
-      lastName: 'Alekseev',
-      location: {country: 'Russia', city: 'Ufa'},
-      status: 'I\'m fine'
-    },
-    {
-      id: 5,
-      followed: false,
-      firstName: 'Ivan',
-      lastName: 'Alekseev',
-      location: {country: 'Russia', city: 'Kazan'},
-      status: 'I\'m fine'
-    },
-    {
-      id: 6,
-      followed: true,
-      firstName: 'Ivan',
-      lastName: 'Alekseev',
-      location: {country: 'England', city: 'London'},
-      status: 'I\'m fine'
-    },
-    {
-      id: 7,
-      followed: false,
-      firstName: 'Ivan',
-      lastName: 'Alekseev',
-      location: {country: 'Russia', city: 'Tomsk'},
-      status: 'I\'m fine'
-    }
+    // {
+    //   id: 1,
+    //   followed: true,
+    //   firstName: 'Ivan',
+    //   lastName: 'Alekseev',
+    //   location: {country: 'Russia', city: 'Tomsk'},
+    //   status: 'I\'m fine'
+    // },
+    // {
+    //   id: 2,
+    //   followed: false,
+    //   firstName: 'Alex',
+    //   lastName: 'Alekseev',
+    //   location: {country: 'Russia', city: 'Tomsk'},
+    //   status: 'I\'m fine'
+    // },
+    // {
+    //   id: 3,
+    //   followed: false,
+    //   firstName: 'Sam',
+    //   lastName: 'Alekseev',
+    //   location: {country: 'Russia', city: 'Moscow'},
+    //   status: 'I\'m fine'
+    // },
+    // {
+    //   id: 4,
+    //   followed: true,
+    //   firstName: 'John',
+    //   lastName: 'Alekseev',
+    //   location: {country: 'Russia', city: 'Ufa'},
+    //   status: 'I\'m fine'
+    // },
+    // {
+    //   id: 5,
+    //   followed: false,
+    //   firstName: 'Ivan',
+    //   lastName: 'Alekseev',
+    //   location: {country: 'Russia', city: 'Kazan'},
+    //   status: 'I\'m fine'
+    // },
+    // {
+    //   id: 6,
+    //   followed: true,
+    //   firstName: 'Ivan',
+    //   lastName: 'Alekseev',
+    //   location: {country: 'England', city: 'London'},
+    //   status: 'I\'m fine'
+    // },
+    // {
+    //   id: 7,
+    //   followed: false,
+    //   firstName: 'Ivan',
+    //   lastName: 'Alekseev',
+    //   location: {country: 'Russia', city: 'Tomsk'},
+    //   status: 'I\'m fine'
+    // }
   ]
 }
 
